f5d241579880e2ac6a0943d6c92295d90bc8efc7
Charles Harper <98134952+VexWasHere@users.noreply.github.com>
2024-08-29 09:01:35 -0400

    Speech recognition function removed
    Due to recent complications, the speech recognition program will be temporarily relocated.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import customtkinter as ctk
 import speech_recognition as sr
+import pyaudio
 
 class TopNav(ctk.CTkFrame):
     def __init__(self, master):
@@ -78,25 +79,3 @@ top_nav = TopNav(root)
 top_nav.pack(fill="both", expand=True)
 
 root.mainloop()
-
-def listen_for_command():
-    recognizer = sr.Recognizer()
-    microphone = sr.Microphone()
-
-    with microphone as source:
-        print("Listening for command...")
-        recognizer.adjust_for_ambient_noise(source)
-
-        audio = recognizer.listen(source, timeout=None, phrase_time_limit=None)
-
-    try:
-        command = recognizer.recognize_google(audio)
-        print(f"You said: {command}")
-        return command
-    except sr.UnknownValueError:
-        print("Sorry, I didn't understand you.")
-    except sr.RequestError:
-        print("Sorry, there's an error with the speech recognition service.")
-
-
-listen_for_command()
